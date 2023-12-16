6769249af0bb8c9a7c7d947900aa3e20c344d0e8
Marxu <fuhaojun@52tt.com>
2023-12-16 22:28:33 +0800

    feat: 更新 product 路由

--- E-commerce-website/backend/src/routes/userRoutes.js
+++ E-commerce-website/backend/src/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const productController = require('../controllers/productController');
 
 // 定义用户相关的路由
 router.post('/login', userController.loginUser);
 router.post('/register', userController.registerUser);
+
+// Product
+router.post('/login', productController.getAllProducts);
 
 module.exports = router;
