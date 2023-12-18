debf47c7edf6e96b3e262e14245329f2ab1f17f8
Marxu <fuhaojun@52tt.com>
2023-12-18 18:33:42 +0800

    feat: 添加 openai 实现识别回复

--- Build-AI-Chat/index.js
+++ Build-AI-Chat/index.js
@@ -1,11 +1,51 @@
-const express = require('express')
-const app = express()
+'use strict';
 
-app.use(express.static(__dirname, '/views')) // html
-app.use(express.static(__dirname,'/public')) // js, css, images
+require('dotenv').config();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY; // Replace with your OpenAI API key
 
-const server = app.listen(5000)
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html')
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const OpenAIApi = require('openai');
+
+const openai = new OpenAIApi({
+    OPENAI_API_KEY
 })
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', async (text) => {
+    console.log('Message: ' + text);
+
+    try {
+      // Get a reply from OpenAI GPT-3
+      const response = await openai.complete({
+        engine: 'gpt-3.5-turbo',
+        prompt: `前端说：${text}`, // Include the user's input in the prompt
+        max_tokens: 150 // Control the length of the reply
+      });
+
+      const aiText = response.choices[0].text.trim();
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
